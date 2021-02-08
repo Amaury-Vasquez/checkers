87b26ad7c1ce76d89ccc9b16123fb89a4bd1e481
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-08 14:49:52 -0600

    deleting innecesary variables

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -3,21 +3,10 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
   html {
     box-sizing: border-box;
-    --light-blue: #bbf1fa;
-    --main: #0f3460;
-    --dark-blue: #1a1a2e;
-    --blue: #51c2d5;
     --brown: #663f3f;
-    --red: #ec4646;
     --dark-square: rgb(184,139,74);
     --white-square: rgb(227,193,111);
-    --white: #eeeeee;
-    --light-gray: #e8e8e8;
-    --gray: #bbbfca;
-    --dark-gray: #495464;
-    --black: #000000;
     --rgba-dark-blue: rgba(26, 26, 46, 0.5);
-    --rgba-blue:  rgba(22, 33, 62, 0.2);
     --rgba-red: rgba(233, 69, 96, 0.5);
     --shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   }
